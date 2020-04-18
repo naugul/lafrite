dfbfaea7c360cb9a1b373741e0e238f428a654f1
Diego Martin Oliva <=>
2020-04-18 11:06:44 -0300

    Corrected platform.dtb name...

--- KernelUpdate.sh
+++ KernelUpdate.sh
@@ -3,7 +3,7 @@ echo "Running update and upgrade..."
 apt-get update
 apt-get upgrade -y
 echo "Upgrade finished, replacing platform.dtb..."
-cp ./platform.dtb /boot/efi/dtb/libre-computer/aml-s805x-ac/platform.dtb
+cp ./meson-gxl-s805x-libretech-ac.dtb /boot/efi/dtb/libre-computer/aml-s805x-ac/platform.dtb
 echo "Installing Headers..."
 dpkg -i linux-headers-5.7.0-rc1_5.7.0-rc1-1_arm64.deb
 echo "Installing Image..."
